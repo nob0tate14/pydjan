c8efe30bf38b2517deee690f75d598fe33c2ca59
nob0tate14 <nob0tate14@gmail.com>
2018-01-22 22:21:20 +0900

    use template.

--- app/exp1/urls.py
+++ app/exp1/urls.py
@@ -6,4 +6,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('page1', views.page1, name='page1'),
 ]
--- app/exp1/views.py
+++ app/exp1/views.py
@@ -1,14 +1,21 @@
 import django
 from django.http.response import HttpResponse
 from django.template import loader
-
+from django.shortcuts import render
 
 # Create your views here.
-def index(request):
-    template = loader.get_template('exp1/index.html')
-    # return HttpResponse("Hello, world. You're at the example 1 index.")
 
+
+def index(request):
+    # return HttpResponse("Hello, world. You're at the example 1 index.")
+    template = loader.get_template('exp1/index.html')
     context = {
         'd_p': django.__path__,
     }
     return HttpResponse(template.render(context, request))
+
+
+def page1(request):
+    # template = loader.get_template('exp1/page1.html')
+    # return HttpResponse(template.render(None, request))
+    return render(request, 'exp1/page1.html')
