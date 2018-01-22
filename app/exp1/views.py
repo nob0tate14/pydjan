b4f8ee7c6a6ce3f8b8faebb6218123a44c60b8e6
nob0tate14 <nob0tate14@gmail.com>
2018-01-22 21:56:01 +0900

    django lesson: use template file

--- app/exp1/views.py
+++ app/exp1/views.py
@@ -1,6 +1,14 @@
+import django
 from django.http.response import HttpResponse
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the example 1 index.")
+    template = loader.get_template('exp1/index.html')
+    # return HttpResponse("Hello, world. You're at the example 1 index.")
+
+    context = {
+        'd_p': django.__path__,
+    }
+    return HttpResponse(template.render(context, request))
